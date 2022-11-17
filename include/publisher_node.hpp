8df4458c799f0812fe51e83637ad3fd27602112d
roboticistjoseph <jkatak73@terpmail.umd.edu>
2022-11-17 02:32:09 -0500

    [ADD] Adding Server to existing publisher

--- include/publisher_node.hpp
+++ include/publisher_node.hpp
@@ -5,9 +5,21 @@
  * @version 0.1
  * @date 2022-11-16
  * 
- * @copyright Copyright (c) 2022
- * 
- */
+ * @copyright 
+ *  // Copyright 2016 Open Source Robotics Foundation, Inc.
+    //
+    // Licensed under the Apache License, Version 2.0 (the "License");
+    // you may not use this file except in compliance with the License.
+    // You may obtain a copy of the License at
+    //
+    //     http://www.apache.org/licenses/LICENSE-2.0
+    //
+    // Unless required by applicable law or agreed to in writing, software
+    // distributed under the License is distributed on an "AS IS" BASIS,
+    // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    // See the License for the specific language governing permissions and
+    // limitations under the License.
+  */
 
 #ifndef BEGINNER_TUTORIALS_INCLUDE_PUBLISHER_NODE_HPP_
 #define BEGINNER_TUTORIALS_INCLUDE_PUBLISHER_NODE_HPP_
@@ -17,11 +29,28 @@
 #include <memory>
 
 // ros libraries
+#include <rclcpp/logging.hpp>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+// service includes
+#include "example_interfaces/srv/add_two_ints.hpp"
+
 // namespaces
-using namespace std::chrono_literals;
+using namespace std::chrono_literals;  // for use of time units: "ms", "s"
+using std::placeholders::_1;           // for use with binding Class member
+using std::placeholders::_2;           // callback function
+
+// topic types
+using STRING    = std_msgs::msg::String;
+using PUBLISHER = rclcpp::Publisher<STRING>::SharedPtr;
+using TIMER     = rclcpp::TimerBase::SharedPtr;
+
+// service types
+using SERVICE    = rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr;
+using ADDTWOINTS = example_interfaces::srv::AddTwoInts;
+using REQUEST    = const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request>;
+using RESPOSE    = std::shared_ptr<example_interfaces::srv::AddTwoInts::Response>;
 
 /**
  * @brief Publisher class.
@@ -29,15 +58,28 @@ using namespace std::chrono_literals;
  */
 class Publisher : public rclcpp::Node {
  public:
+  //////////////////////////
+  // Constructor
+  //////////////////////////
     Publisher();
 
  private:
+  //////////////////////////
+  // Member Variables:
+  //////////////////////////
+  size_t    m_count_;
+  PUBLISHER m_publisher_;
+  TIMER     m_timer_;
+  SERVICE   m_service_;
+
+  //////////////////////////
+  // Member Functions:
+  //////////////////////////
   void timer_callback();
+  void add(REQUEST request, RESPOSE response);
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   size_t count_;
 };
-
-// RCLCPP_COMPONENTS_REGISTER_NODE(demo_nodes_cpp::Talker)
 
 #endif  // BEGINNER_TUTORIALS_INCLUDE_PUBLISHER_NODE_HPP_
--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -5,30 +5,124 @@
  * @version 0.1
  * @date 2022-11-16
  * 
- * @copyright Copyright (c) 2022
- * 
+ * @copyright 
+ *  // Copyright 2016 Open Source Robotics Foundation, Inc.
+    //
+    // Licensed under the Apache License, Version 2.0 (the "License");
+    // you may not use this file except in compliance with the License.
+    // You may obtain a copy of the License at
+    //
+    //     http://www.apache.org/licenses/LICENSE-2.0
+    //
+    // Unless required by applicable law or agreed to in writing, software
+    // distributed under the License is distributed on an "AS IS" BASIS,
+    // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    // See the License for the specific language governing permissions and
+    // limitations under the License.
+  */
+
+/*
+ * A program combining publisher and service server.
+ *
+ * How to run:
+ *  1.) Start the publisher and service server.  (terminal 1)
+ *     ros2 run beginner_tutorials publisher_node
+ *
+ *  2.) Send a service request. (terminal 2)
+ *     ros2 service call /add_two_ints_v2 srv/AddTwoInts "{a: 1, b: 2}"
+ *
+ * Expected output:
+ *  terminal 1 :
+ *    [minimal_publisher]: Publishing: Hello, world! 846
+ *    [minimal_publisher]: Publishing: Hello, world! 847
+ *    [minimal_publisher]: Incoming request
+ *    a: 1 b: 2
+ *    [minimal_publisher]: sending back response: [3]
+ *    [minimal_publisher]: Publishing: Hello, world! 848
+ *    [minimal_publisher]: Publishing: Hello, world! 849
+ *
+ *  terminal 2 :
+ *    requester: making request: example_interfaces.srv.AddTwoInts_Request(a=1, b=2)
+ *    
+ *    response:
+ *    example_interfaces.srv.AddTwoInts_Response(sum=3)
  */
 
 #include "../include/publisher_node.hpp"
 
 Publisher::Publisher()
     : Node("publisher"), count_(0) {
-       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-       timer_ = this->create_wall_timer(500ms,
-        std::bind(&Publisher::timer_callback, this));
+      /* 
+       * Create publisher with buffer size of 10 and frequency = 2 hz
+       */
+       auto topicName = "topic";
+       publisher_ = this->create_publisher<std_msgs::msg::String>(topicName, 10);
+       auto topicCallbackPtr = std::bind(&Publisher::timer_callback, this);
+       timer_ = this->create_wall_timer(500ms, topicCallbackPtr);
+
+       /*
+        * Creates a service server (with a service name = "add_two_ints_v2")
+        */
+       auto serviceName = "add_two_ints_v2";
+       auto serviceCallbackPtr = std::bind(&Publisher::add, this, _1, _2);
+       m_service_ = create_service <ADDTWOINTS>(serviceName, serviceCallbackPtr);
 }
 
 void Publisher::timer_callback() {
+    // Create the message to publish
     auto message = std_msgs::msg::String();
     message.data = "Developer- Joseph:  "
              + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+
+    // Publish the message
     publisher_->publish(message);
+
+    // Logger level- Debug (Debug messages detail the entire step-by-step process of the system execution.)
+    RCLCPP_DEBUG_STREAM(this->get_logger(), "Logger level: Debug, Publisher processing");
+    // Logger level- Info (Info messages indicate event and status updates that serve as a
+    // visual verification that the system is running as expected.)
+    RCLCPP_INFO_STREAM(this->get_logger(), "Logger level: Info, Publishing:" << message.data.c_str());
+
+    // Logger level - Warning (Warn messages indicate unexpected activity or non-ideal
+    // results that might represent a deeper issue, but don’t harm functionality outright.)
+    if (count_ > 5) {
+      RCLCPP_WARN_STREAM(this->get_logger(), "Logger level: Warning, Too many Publising cycles");
+      }
+
+    // Logger level - Fatal (Fatal messages indicate the system is going to terminate
+    // to try to protect itself from detriment.)
+    if (count_ > 10) {
+      RCLCPP_FATAL_STREAM(this->get_logger(), "Logger level: Fatal, Fatal error due to overuse");
+    }
+}
+
+void Publisher::add(REQUEST request,
+            RESPOSE response) {
+    response->sum = request->a + request->b;
+
+    // Logger level- Info
+    RCLCPP_INFO(this->get_logger(), "Incoming request\na: %ld b: %ld", request->a, request->b);
+    RCLCPP_INFO(this->get_logger(), "sending back response: [%ld]", (long int) response->sum);
+
+    if (response->sum != request->a + request->b) {
+      // Logger level - Error (Error messages indicate significant issues that won’t
+      // necessarily damage the system, but are preventing it from functioning properly.)
+      RCLCPP_ERROR_STREAM(this->get_logger(), "Logger level: Error, Incorrect Calculation"
+                          << std::to_string(invalid++));
+      return;
+  }
 }
 
 int main(int argc, char * argv[]) {
+  // 1.) Initialize ROS 2 C++ client library
   rclcpp::init(argc, argv);
+
+  // 2.) Start processing
   rclcpp::spin(std::make_shared<Publisher>());
+
+  // 3.) Shutdown ROS 2
   rclcpp::shutdown();
+
   return 0;
 }
